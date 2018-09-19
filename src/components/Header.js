7f6ad3b337a380a6c75b403acbc0a6b90e4298c4
Antony Laroche <antony@larochefamily.net>
2018-09-18 22:11:16 -0500

    added props to components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types"; //used to validate type of prop (i.e. string, object, integer, etc)
 
-const Header = () => {
+//functional component
+const Header = props => {
+  const { branding } = props; // deconstructing props to simplify code, otherwise will have to use props.branding
   return (
     <div>
-      <h1>Contact Manager</h1>
+      <h1>{branding}</h1>
     </div>
   );
+};
+
+// default props are used to set props when no props are passed in when functional component is called
+Header.defaultProps = {
+  branding: "My App"
+};
+
+Header.PropTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;
