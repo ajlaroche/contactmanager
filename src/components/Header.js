b63516a1c78c9f1b9196d34fb86eca2c5f4cdc13
Antony Laroche <antony@larochefamily.net>
2018-09-18 22:44:45 -0500

    added bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,26 @@
 import React from "react";
-import PropTypes from "prop-types"; //used to validate type of prop (i.e. string, object, integer, etc)
+import propTypes from "prop-types"; //used to validate type of prop (i.e. string, object, integer, etc)
 
 //functional component
 const Header = props => {
   const { branding } = props; // deconstructing props to simplify code, otherwise will have to use props.branding
   return (
-    <div>
-      <h1>{branding}</h1>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+      <div className="container">
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
+        <div>
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <a href="/" className="nav-link">
+                Home
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
@@ -16,8 +29,8 @@ Header.defaultProps = {
   branding: "My App"
 };
 
-Header.PropTypes = {
-  branding: PropTypes.string.isRequired
+Header.propTypes = {
+  branding: propTypes.string.isRequired
 };
 
 export default Header;
