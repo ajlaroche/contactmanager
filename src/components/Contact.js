2e3e03d491754a86e611c63ad60fc0d522f922c1
Antony Laroche <antony@larochefamily.net>
2018-09-19 21:49:49 -0500

    added states and dynamic rendering

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,24 +2,40 @@ import React, { Component } from "react";
 import propTypes from "prop-types";
 
 class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+      //   instead of using false, just switch to opposite or equivalent to a toggle
+    });
+  };
+
   render() {
-    const { name, email, phone } = this.props; // deconstructing props to simplify code, otherwise will have to use this.props.name, this.props.email, this.props.phone
+    const { contact } = this.props; // deconstructing props to simplify code, otherwise will have to use this.props.name, this.props.email, this.props.phone
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {contact.name}{" "}
+          <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {contact.email}</li>
+            <li className="list-group-item">Phone: {contact.phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
 }
 
 Contact.propTypes = {
-  name: propTypes.string.isRequired,
-  email: propTypes.string.isRequired,
-  phone: propTypes.string.isRequired
+  contact: propTypes.object.isRequired
 };
 
 export default Contact;
