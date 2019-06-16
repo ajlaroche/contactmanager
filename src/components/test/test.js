98f19f28d356f48b5c12f4dc42a4dcb9fc212832
Antony Laroche <antony@larochefamily.net>
2019-06-15 19:40:21 -0500

    get request to get users

--- src/components/test/test.js
+++ src/components/test/test.js
@@ -2,29 +2,40 @@ import React, { Component } from "react";
 
 class test extends Component {
   state = {
-    test: "test"
+    title: "",
+    body: ""
   };
 
   componentDidMount() {
-    console.log("component did mount ....");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({
+          title: data.title,
+          body: data.body
+        });
+      });
   }
 
-  componentWillMount() {
-    console.log("component will mount ....");
-  }
+  // componentWillMount() {
+  //   console.log("component will mount ....");
+  // }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log("compone wil receive props...");
-  }
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log("compone wil receive props...");
+  // }
 
-  static getDerivedStateFromProps(nextProps, nextState) {
-    return null;
-  }
+  // static getDerivedStateFromProps(nextProps, nextState) {
+  //   return null;
+  // }
 
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
